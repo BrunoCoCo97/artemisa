4fa98f05a42056a7bc41d0ff32d289f78ef8bc7e
Gustavo Adolfo Arellano Sandoval <arellano.gustavo@gmail.com>
2023-01-23 10:41:20 -0600

    Create corre.sh

---
+++ corre.sh
@@ -0,0 +1,4 @@
+curl -X POST "https://access.qbits.mx/api/login" -H  "accept: 
+application/json;charset=utf-8" -H  "Content-Type: application/json" -d "{  
+\"cred\": \"holaHola\",  \"hash\": 0,  \"user\": 
+\"arellano.gustavo@gmail.com\"}"
